f65b129df181ecce4cd31645c21cb2026303a688
fancyIX <fancyIX@yahoo.com>
2021-11-08 19:21:16 -0800

    Data correct for high end

--- lyra2/cuda_lyra2.cu
+++ lyra2/cuda_lyra2.cu
@@ -69,6 +69,30 @@
          pad[col / 2][row][2] = data[2];
  }
 
+ __device__ __forceinline__ void LD4SG(uint2 res[3], const int row, const int col, const int thread, const int threads, const int offset, uint64_t *GPad, uint64_t *GPadO)
+ {
+    ulong *p = (ulong *) ((GPad + 2 * (offset + (((col /2) * 64 + row * 64 * (Ncol / 2))))));
+    ulong *q = (ulong *) (GPadO + offset + (((col /2) * 64 + row * 64 * (Ncol / 2))));
+     /*uint4 tmp = __ldg(p);
+     res[0] = make_uint2(tmp.x, tmp.y);
+     res[1] = make_uint2(tmp.z, tmp.w);
+     res[2] = __ldg(q);*/
+     ulong * r = (ulong *)(&res[0]);
+     asm ("ld.global.u64 %2, [%4];\n\tld.global.v2.u64 {%0, %1}, [%3];"
+     : "=l"(r[0]), "=l"(r[1]), "=l"(r[2]) : "l"(p), "l"(q) : "memory");
+ }
+
+ __device__ __forceinline__ void ST4SG(const int row, const int col, const uint2 data[3], const int thread, const int threads, const int offset, uint64_t *GPad, uint64_t *GPadO)
+ {
+     ulong a = MAKE_ULONGLONG(data[0].x, data[0].y);
+     ulong b = MAKE_ULONGLONG(data[1].x, data[1].y);
+     ulong c = MAKE_ULONGLONG(data[2].x, data[2].y);
+     ulong *p = (ulong *) ((GPad + 2 * (offset + (((col /2) * 64 + row * 64 * (Ncol / 2))))));
+     ulong *q = (ulong *) (GPadO + offset + (((col /2) * 64 + row * 64 * (Ncol / 2))));
+     asm ("st.global.v2.u64 [%0], {%2, %3};\n\t st.global.u64 [%1], %4;\n\t" :: "l"(p), "l"(q), "l"(a), "l"(b), "l"(c): "memory");
+    //((uint4*)GPad)[offset + ((threadIdx.x + 4 * threadIdx.y + (col /2) * 64 + row * 64 * (Ncol / 2)))] = tmp;
+    //GPad[offset + ((threadIdx.x + 4 * threadIdx.y + (col /2) * 64 + row * 64 * (Ncol / 2))) + 64 * Nrow * (Ncol / 2) * 2] = data[2];
+ }
  
  #if __CUDA_ARCH__ >= 300
  __device__ __forceinline__ uint32_t WarpShuffle(uint32_t a, uint32_t b, uint32_t c)
@@ -481,6 +505,328 @@
      for (int j = 0; j < 3; j++)
          state[j] ^= last[j];
  }
+
+ // ================================= high end ==========================
+ static __device__ __forceinline__
+ void reduceDuplex_high_end(uint2 state[4], uint32_t thread, const uint32_t threads, uint64_t *g_pad)
+ {
+
+    const int offset = blockIdx.x * 64 * Nrow * (Ncol / 2) * 3 + threadIdx.x + 4 * threadIdx.y;
+    uint64_t *g_pado = g_pad + 64 * Nrow * (Ncol / 2) * 2;
+
+     uint2 state1[3];
+	 uint2 state2[3];
+ 
+
+     for (int i = 0; i < Nrow; i++)
+     {
+         if ((i & 1) == 1)
+         ST4SS(0, Ncol - i - 1, state, thread, threads);
+         else
+         ST4SG(0, Ncol - i - 1, state, thread, threads, offset, g_pad, g_pado);
+ 
+         round_lyra(state);
+     }
+ 
+     for (int i = 0; i < Nrow; i+=2)
+     {
+         LD4SS(state1, 0, i, thread, threads);
+		 LD4SG(state2, 0, i + 1, thread, threads, offset, g_pad, g_pado);
+		 #pragma unroll
+         for (int j = 0; j < 3; j++)
+             state[j] ^= state1[j];
+ 
+         round_lyra(state);
+ 
+		 #pragma unroll
+         for (int j = 0; j < 3; j++)
+             state1[j] ^= state[j];
+		
+			 #pragma unroll
+         for (int j = 0; j < 3; j++)
+             state[j] ^= state2[j];
+ 
+         round_lyra(state);
+ 
+		 #pragma unroll
+         for (int j = 0; j < 3; j++)
+             state2[j] ^= state[j];
+         ST4SG(1, Ncol - i - 1, state1, thread, threads, offset, g_pad, g_pado);
+		 ST4SS(1, Ncol - (i + 1) - 1, state2, thread, threads);
+     }
+ }
+ 
+ static __device__ __forceinline__
+ void reduceDuplexRowSetup_high_end(const int rowIn, const int rowInOut, const int rowOut, uint2 state[4], uint32_t thread, const uint32_t threads, uint64_t *g_pad)
+ {
+     const int offset = blockIdx.x * 64 * Nrow * (Ncol / 2) * 3 + threadIdx.x + 4 * threadIdx.y;
+     uint64_t *g_pado = g_pad + 64 * Nrow * (Ncol / 2) * 2;
+
+     uint2 state1[3], state2[3], state3[3], state4[3];
+
+     for (int i = 0; i < Nrow; i+=2)
+     {
+         LD4SS(state1, rowIn, i, thread, threads);
+		 LD4SS(state2, rowInOut, i, thread, threads);
+		 LD4SG(state3, rowIn, i + 1, thread, threads, offset, g_pad, g_pado);
+		 LD4SG(state4, rowInOut, i + 1, thread, threads, offset, g_pad, g_pado);
+		 #pragma unroll
+         for (int j = 0; j < 3; j++)
+             state[j] ^= state1[j] + state2[j];
+ 
+         round_lyra(state);
+ 
+         #pragma unroll
+         for (int j = 0; j < 3; j++)
+             state1[j] ^= state[j];
+ 
+         ST4SG(rowOut, Ncol - i - 1, state1, thread, threads, offset, g_pad, g_pado);
+ 
+         // simultaneously receive data from preceding thread and send data to following thread
+         uint2 Data0 = state[0];
+         uint2 Data1 = state[1];
+         uint2 Data2 = state[2];
+         WarpShuffle3(Data0, Data1, Data2, threadIdx.x - 1, threadIdx.x - 1, threadIdx.x - 1, 4);
+ 
+         if (threadIdx.x == 0)
+         {
+             state2[0] ^= Data2;
+             state2[1] ^= Data0;
+             state2[2] ^= Data1;
+         } else {
+             state2[0] ^= Data0;
+             state2[1] ^= Data1;
+             state2[2] ^= Data2;
+         }
+ 
+         ST4SS(rowInOut, i, state2, thread, threads);
+
+		//=====================================
+		 #pragma unroll
+         for (int j = 0; j < 3; j++)
+             state[j] ^= state3[j] + state4[j];
+ 
+         round_lyra(state);
+ 
+         #pragma unroll
+         for (int j = 0; j < 3; j++)
+             state3[j] ^= state[j];
+ 
+         ST4SS(rowOut, Ncol - (i + 1) - 1, state3, thread, threads);
+ 
+         // simultaneously receive data from preceding thread and send data to following thread
+         uint2 Data01 = state[0];
+         uint2 Data11 = state[1];
+         uint2 Data21 = state[2];
+         WarpShuffle3(Data01, Data11, Data21, threadIdx.x - 1, threadIdx.x - 1, threadIdx.x - 1, 4);
+ 
+         if (threadIdx.x == 0)
+         {
+             state4[0] ^= Data21;
+             state4[1] ^= Data01;
+             state4[2] ^= Data11;
+         } else {
+             state4[0] ^= Data01;
+             state4[1] ^= Data11;
+             state4[2] ^= Data21;
+         }
+ 
+         ST4SG(rowInOut, (i + 1), state4, thread, threads, offset, g_pad, g_pado);
+     }
+ }
+ 
+ static __device__ __forceinline__
+ void reduceDuplexRowt_high_end(const int rowIn, const int rowInOut, const int rowOut, uint2 state[4], const uint32_t thread, const uint32_t threads, uint64_t *g_pad)
+ {
+     const int offset = blockIdx.x * 64 * Nrow * (Ncol / 2) * 3 + threadIdx.x + 4 * threadIdx.y;
+     uint64_t *g_pado = g_pad + 64 * Nrow * (Ncol / 2) * 2;
+
+     uint2 state3[3], state4[3], state5[3];
+
+     LD4SG(state3, rowIn, 0 + 1, thread, threads, offset, g_pad, g_pado);
+     LD4SG(state4, rowInOut, 0 + 1, thread, threads, offset, g_pad, g_pado);
+     LD4SG(state5, rowOut, 0 + 1, thread, threads, offset, g_pad, g_pado);
+
+     for (int i = 0; i < Nrow; i+=2)
+     {
+         uint2 state1[3], state2[3], state7[3], state8[3], state9[3];
+ 
+         LD4SS(state1, rowIn, i, thread, threads);
+         LD4SS(state2, rowInOut, i, thread, threads);
+		 
+ #pragma unroll
+         for (int j = 0; j < 3; j++)
+             state[j] ^= state1[j] + state2[j];
+ 
+         LD4SS(state1, rowOut, i, thread, threads);
+
+         round_lyra(state);
+         LD4SG(state7, rowIn, i + 3, thread, threads, offset, g_pad, g_pado);
+         LD4SG(state8, rowInOut, i + 3, thread, threads, offset, g_pad, g_pado);
+         LD4SG(state9, rowOut, i + 3, thread, threads, offset, g_pad, g_pado);
+ 
+         // simultaneously receive data from preceding thread and send data to following thread
+         uint2 Data0 = state[0];
+         uint2 Data1 = state[1];
+         uint2 Data2 = state[2];
+         WarpShuffle3(Data0, Data1, Data2, threadIdx.x - 1, threadIdx.x - 1, threadIdx.x - 1, 4);
+ 
+         if (threadIdx.x == 0)
+         {
+             state2[0] ^= Data2;
+             state2[1] ^= Data0;
+             state2[2] ^= Data1;
+         }
+         else
+         {
+             state2[0] ^= Data0;
+             state2[1] ^= Data1;
+             state2[2] ^= Data2;
+         }
+
+        if (rowInOut != rowOut) {
+             ST4SS(rowInOut, i, state2, thread, threads);
+                 #pragma unroll
+            for (int j = 0; j < 3; j++)
+                state2[j] = state1[j];
+         }
+
+#pragma unroll
+        for (int j = 0; j < 3; j++)
+            state2[j] ^= state[j];
+
+        ST4SS(rowOut, i, state2, thread, threads);
+
+		 //======================================
+ 
+ 
+ #pragma unroll
+         for (int j = 0; j < 3; j++)
+             state[j] ^= state3[j] + state4[j];
+
+         round_lyra(state);
+ 
+         // simultaneously receive data from preceding thread and send data to following thread
+         uint2 Data01 = state[0];
+         uint2 Data11 = state[1];
+         uint2 Data21 = state[2];
+         WarpShuffle3(Data01, Data11, Data21, threadIdx.x - 1, threadIdx.x - 1, threadIdx.x - 1, 4);
+ 
+         if (threadIdx.x == 0)
+         {
+             state4[0] ^= Data21;
+             state4[1] ^= Data01;
+             state4[2] ^= Data11;
+         }
+         else
+         {
+             state4[0] ^= Data01;
+             state4[1] ^= Data11;
+             state4[2] ^= Data21;
+         }
+
+         if (rowInOut != rowOut) {
+             ST4SG(rowInOut, i + 1, state4, thread, threads, offset, g_pad, g_pado);
+                 #pragma unroll
+            for (int j = 0; j < 3; j++)
+                state4[j] = state5[j];
+         }
+
+#pragma unroll
+        for (int j = 0; j < 3; j++)
+            state4[j] ^= state[j];
+
+        ST4SG(rowOut, i + 1, state4, thread, threads, offset, g_pad, g_pado);
+
+        #pragma unroll
+        for (int j = 0; j < 3; j++)
+            state3[j] = state7[j];
+        #pragma unroll
+        for (int j = 0; j < 3; j++)
+            state4[j] = state8[j];
+        #pragma unroll
+        for (int j = 0; j < 3; j++)
+            state5[j] = state9[j];
+     }
+ }
+ 
+ static __device__ __forceinline__
+ void reduceDuplexRowt_8_high_end(const int rowInOut, uint2* state, const uint32_t thread, const uint32_t threads, uint64_t *g_pad)
+ {
+     const int offset = blockIdx.x * 64 * Nrow * (Ncol / 2) * 3 + threadIdx.x + 4 * threadIdx.y;
+     uint64_t *g_pado = g_pad + 64 * Nrow * (Ncol / 2) * 2;
+
+     uint2 state1[3], state2[3], state3[3], state4[3], last[3];
+ 
+     LD4SS(state1, 2, 0, thread, threads);
+     LD4SS(last, rowInOut, 0, thread, threads);
+ 
+     #pragma unroll
+     for (int j = 0; j < 3; j++)
+         state[j] ^= state1[j] + last[j];
+ 
+     round_lyra(state);
+ 
+     // simultaneously receive data from preceding thread and send data to following thread
+     uint2 Data0 = state[0];
+     uint2 Data1 = state[1];
+     uint2 Data2 = state[2];
+     WarpShuffle3(Data0, Data1, Data2, threadIdx.x - 1, threadIdx.x - 1, threadIdx.x - 1, 4);
+ 
+     if (threadIdx.x == 0)
+     {
+         last[0] ^= Data2;
+         last[1] ^= Data0;
+         last[2] ^= Data1;
+     } else {
+         last[0] ^= Data0;
+         last[1] ^= Data1;
+         last[2] ^= Data2;
+     }
+ 
+     if (rowInOut == 5)
+     {
+         #pragma unroll
+         for (int j = 0; j < 3; j++)
+             last[j] ^= state[j];
+     }
+ 
+	 LD4SG(state1, 2, 1, thread, threads, offset, g_pad, g_pado);
+	 LD4SG(state2, rowInOut, 1, thread, threads, offset, g_pad, g_pado);
+
+	 #pragma unroll
+	 for (int j = 0; j < 3; j++)
+		 state[j] ^= state1[j] + state2[j];
+
+	 round_lyra(state);
+
+     for (int i = 2; i < Nrow; i+=2)
+     {
+         LD4SS(state1, 2, i, thread, threads);
+         LD4SS(state2, rowInOut, i, thread, threads);
+		 LD4SG(state3, 2, i + 1, thread, threads, offset, g_pad, g_pado);
+         LD4SG(state4, rowInOut, i + 1, thread, threads, offset, g_pad, g_pado);
+ 
+         #pragma unroll
+         for (int j = 0; j < 3; j++)
+             state[j] ^= state1[j] + state2[j];
+ 
+         round_lyra(state);
+
+		 //============================
+ 
+         #pragma unroll
+         for (int j = 0; j < 3; j++)
+             state[j] ^= state3[j] + state4[j];
+ 
+         round_lyra(state);
+     }
+ 
+     #pragma unroll
+     for (int j = 0; j < 3; j++)
+         state[j] ^= last[j];
+ }
+ // ================================= end high ==========================
  
  __constant__ uint2x4 blake2b_IV[2] = {
      0xf3bcc908lu, 0x6a09e667lu,
@@ -562,6 +908,47 @@
      }
  }
  
+__global__
+ __launch_bounds__(64, 1)
+ void lyra2_gpu_hash_32_2_high_end(uint32_t threads, uint32_t startNounce, uint64_t *g_pad)
+ {
+     const uint32_t thread = blockDim.y * blockIdx.x + threadIdx.y;
+ 
+     if (thread < threads)
+     {
+         uint2 state[4];
+         state[0] = __ldg(&DMatrix[(0 * threads + thread) * blockDim.x + threadIdx.x]);
+         state[1] = __ldg(&DMatrix[(1 * threads + thread) * blockDim.x + threadIdx.x]);
+         state[2] = __ldg(&DMatrix[(2 * threads + thread) * blockDim.x + threadIdx.x]);
+         state[3] = __ldg(&DMatrix[(3 * threads + thread) * blockDim.x + threadIdx.x]);
+ 
+         reduceDuplex_high_end(state, thread, threads, g_pad);
+         reduceDuplexRowSetup_high_end(1, 0, 2, state, thread, threads, g_pad);
+         reduceDuplexRowSetup_high_end(2, 1, 3, state, thread, threads, g_pad);
+         reduceDuplexRowSetup_high_end(3, 0, 4, state, thread, threads, g_pad);
+         reduceDuplexRowSetup_high_end(4, 3, 5, state, thread, threads, g_pad);
+         reduceDuplexRowSetup_high_end(5, 2, 6, state, thread, threads, g_pad);
+         reduceDuplexRowSetup_high_end(6, 1, 7, state, thread, threads, g_pad);
+
+         uint32_t rowa;
+         uint32_t row = 0;
+         uint32_t pre = 7;
+         for (int i = 0; i < 7; i++) {
+            rowa = WarpShuffle(state[0].x, 0, 4) & 7;
+            reduceDuplexRowt_high_end(pre, rowa, row, state, thread, threads, g_pad);
+            pre = row;
+            row = (row + 3) % 8;
+         }
+         rowa = WarpShuffle(state[0].x, 0, 4) & 7;
+         reduceDuplexRowt_8_high_end(rowa, state, thread, threads, g_pad);
+ 
+         DMatrix[(0 * threads + thread) * blockDim.x + threadIdx.x] = state[0];
+         DMatrix[(1 * threads + thread) * blockDim.x + threadIdx.x] = state[1];
+         DMatrix[(2 * threads + thread) * blockDim.x + threadIdx.x] = state[2];
+         DMatrix[(3 * threads + thread) * blockDim.x + threadIdx.x] = state[3];
+     }
+ }
+
  __global__ __launch_bounds__(64, 1)
  void lyra2_gpu_hash_32_3(uint32_t threads, uint32_t startNounce, uint2 *g_hash)
  {
@@ -602,9 +989,18 @@ void lyra2_cpu_init(int thr_id, uint32_t threads, uint64_t *d_matrix)
 	// just assign the device pointer allocated in main loop
 	cudaMemcpyToSymbol(DMatrix, &d_matrix, sizeof(uint64_t*), 0, cudaMemcpyHostToDevice);
 }
+__host__
+void lyra2_cpu_init_high_end(int thr_id, uint32_t threads, uint64_t *g_pad)
+{
+}
 
 __host__
-void lyra2_cpu_hash_32(int thr_id, uint32_t threads, uint32_t startNounce, uint64_t *d_hash, bool gtx750ti)
+void lyra2_cpu_hash_32(int thr_id, uint32_t threads, uint32_t startNounce, uint64_t *d_hash, bool gtx750ti, bool high_end)
+{
+}
+
+__host__
+void lyra2_cpu_hash_32_fancyIX(int thr_id, uint32_t threads, uint32_t startNounce, uint64_t *d_hash, uint64_t *g_pad, bool gtx750ti, bool high_end)
 {
 	int dev_id = device_map[thr_id % MAX_GPUS];
 
@@ -627,7 +1023,10 @@ void lyra2_cpu_hash_32(int thr_id, uint32_t threads, uint32_t startNounce, uint6
 	{
 		lyra2_gpu_hash_32_1 <<< grid2, block2 >>> (threads, startNounce, (uint2*)d_hash);
 
-		lyra2_gpu_hash_32_2 <<< grid1, block1, 12 * (8 - 0) * sizeof(uint2) * 64 >>> (threads, startNounce, d_hash);
+        if (high_end)
+            lyra2_gpu_hash_32_2_high_end <<< grid1, block1, 12 * (8 - 0) * sizeof(uint2) * 64 >>> (threads, startNounce, g_pad);
+        else
+		    lyra2_gpu_hash_32_2 <<< grid1, block1, 12 * (8 - 0) * sizeof(uint2) * 64 >>> (threads, startNounce, d_hash);
 
 		lyra2_gpu_hash_32_3 <<< grid2, block2 >>> (threads, startNounce, (uint2*)d_hash);
 	}
