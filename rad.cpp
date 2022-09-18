6192dcf5da61702cf57e21f1fa206ec5190e9301
Decho Kocharin <sourcedecho@outlook.com>
2022-09-18 15:35:24 +0700

    Update rad.cpp

--- rad.cpp
+++ rad.cpp
@@ -6,25 +6,33 @@ extern void rad_cpu_init(int thr_id);
 extern void rad_cpu_hash(int thr_id, uint32_t threads, uint32_t startNounce, const uint64_t *const data, uint32_t *const h_nounce);
 extern void rad_midstate(const uint32_t *data, uint32_t *midstate);
 
-void rad_hash(void *output, const void *input)
+void rad_hash(uint32_t *output, const uint32_t *data, uint32_t nonce)
 {
-	unsigned char _ALIGN(64) hash[64];
-	
+	uint32_t header[20];
+	for (int i = 0; i < 20; i++) {
+		header[i] = swab32(data[i]);
+	}
+	header[19] = swab32(nonce);
 	sph_sha512_context ctx;
 	sph_sha512_256_init(&ctx);
-	sph_sha512(&ctx, input, 80);
-	sph_sha512_close(&ctx, hash);
+	sph_sha512(&ctx, header, 80);
+	uint32_t hash1[16];
+	sph_sha512_close(&ctx, hash1);
 
+	uint32_t hash2[16];
 	sph_sha512_256_init(&ctx);
-	sph_sha512(&ctx, hash, 32);
-	sph_sha512_close(&ctx, output);
-	
+	sph_sha512(&ctx, hash1, 32);
+	sph_sha512_close(&ctx, hash2);
+
+	for (int i = 0; i < 8; i++) {
+		output[i] = hash2[i];
+	}
 }
 
 static bool init[MAX_GPUS] = { 0 };
-
 int scanhash_rad(int thr_id, struct work* work, uint32_t max_nonce, unsigned long *hashes_done)
 {
+	
 	uint32_t *pdata = work->data;
 	uint32_t *ptarget = work->target;
 	const uint32_t first_nonce = pdata[19];
@@ -45,7 +53,6 @@ int scanhash_rad(int thr_id, struct work* work, uint32_t max_nonce, unsigned lon
 			CUDA_LOG_ERROR();
 		}
 		gpulog(LOG_INFO, thr_id, "Intensity set to %g, %u cuda threads", throughput2intensity(throughput), throughput);
-
 		rad_cpu_init(thr_id);
 		init[thr_id] = true;
 	}
@@ -57,7 +64,7 @@ int scanhash_rad(int thr_id, struct work* work, uint32_t max_nonce, unsigned lon
 		{
 			uint32_t vhash64[8]={0};
 
-			rad_hash(vhash64, pdata);
+			rad_hash(vhash64, pdata, work->nonces[0]);
 			if (vhash64[7] == 0 && fulltest(vhash64, ptarget))
 			{
 				int res = 1;
@@ -65,7 +72,7 @@ int scanhash_rad(int thr_id, struct work* work, uint32_t max_nonce, unsigned lon
 				*hashes_done = pdata[19] - first_nonce + throughput;
 				if (work->nonces[1] != 0xffffffff)
 				{
-					rad_hash(vhash64, pdata);
+					rad_hash(vhash64, pdata, work->nonces[1]);
 					if (vhash64[7] == 0 && fulltest(vhash64, ptarget))
 					{
 						pdata[21] = work->nonces[1];
@@ -100,7 +107,7 @@ int scanhash_rad(int thr_id, struct work* work, uint32_t max_nonce, unsigned lon
 		}
 		pdata[19] += throughput;
 	} while (!work_restart[thr_id].restart);
-
+	
 	*hashes_done = pdata[19] - first_nonce ;
 	return 0;
 }
