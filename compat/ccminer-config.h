64acd99c3eeadc535d1ff5f788a25dd220eb283b
Decho Kocharin <sourcedecho@outlook.com>
2025-03-13 16:13:56 +0700

    Update curl-for-windows

--- compat/ccminer-config.h
+++ compat/ccminer-config.h
@@ -158,13 +158,13 @@
 #define PACKAGE_BUGREPORT ""
 
 /* Define to the full name of this package. */
-#define PACKAGE_NAME "ccminer-tpfuemp"
+#define PACKAGE_NAME "ccminer"
 
 /* Define to the home page for this package. */
-#define PACKAGE_URL "https://github.com/tpfuemp/ccminer-tpfuemp"
+#define PACKAGE_URL "http://github.com/tpruvot/ccminer"
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "1.0.0"
+#define PACKAGE_VERSION "2.4.0"
 
 /* If using the C implementation of alloca, define if you know the
    direction of stack growth for your system; otherwise it will be
--- compat/stdbool.h
+++ compat/stdbool.h
@@ -1,6 +1,7 @@
 #pragma once
 
+#ifndef __cplusplus  // Only define these for C
 #define false   0
 #define true    1
-
-#define bool int
+#define bool    int
+#endif
